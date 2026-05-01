7f4cc937f98181ffddb5e0acea57becab6860fef
KuzuJesse508241 <148663913+Jesse5800-2@users.noreply.github.com>
2026-05-02 02:44:38 +0800

    Update config.ts
    改了配置文件

--- src/config.ts
+++ src/config.ts
@@ -8,21 +8,21 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "早川智将的垃圾堆",
+	subtitle: "[Reboot]",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "もう少しだけ,by pixiv@mocha", // Credit text to be displayed
+			url: "https://www.pixiv.net/artworks/91451279", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
@@ -31,11 +31,17 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		 {
+		   src: 'favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		   sizes: '64x64',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		 },
+		  {
+		   src: 'favicon/icon_dark.png',    // Path of the favicon, relative to the /public directory
+		   theme: 'dark',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		   sizes: '64x64',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		 }
+		 
 	],
 };
 
@@ -46,34 +52,44 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/Jesse5800-2/", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "早川智将",
+	bio: "少年,如果没有奇迹,那就自己去创造奇迹就好.",
 	links: [
 		{
 			name: "Twitter",
 			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			url: "https://twitter.com/@NobitaZhen22128",
 		},
+		//{
+		//	name: "Pixiv",
+		//	icon: "simple-icons:pixiv",
+		//	url: "https://www.pixiv.net/users/108076015",
+		//},
 		{
 			name: "Steam",
 			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			url: "https://steamcommunity.com/profiles/76561199519013034/",
 		},
 		{
 			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			icon: "fa6-brands:github-alt",
+			url: "https://github.com/Jesse5800-2",
 		},
+		//{
+		//	name: "bilibili",
+		//	icon: "mingcute:bilibili-line",
+		//	url: "https://space.bilibili.com/3493291999890101",
+		//},
 	],
 };
 
